10ab87ba784dcacbefe11ff5e3ff289135191c98
Javier Villanueva <javier.villanueva@rookmotion.com>
2022-01-14 12:29:25 -0600

    :sparkles: more observables

---
+++ src/operators/08-first.ts
@@ -0,0 +1,13 @@
+// Terminar el observable cuando el primer cumpla la condición
+import { first, fromEvent, pluck } from "rxjs";
+
+// No emite el valor esta que cumpla la emisión
+fromEvent<MouseEvent>( document, 'click')
+  .pipe(
+    pluck('clientY'),
+    first<number>( clientY => clientY >= 150)
+  )
+  .subscribe({
+    next: value => console.log(value),
+    complete: () => console.log('completo')
+  })
